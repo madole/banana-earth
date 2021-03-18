f4f58106ac6067fec776a87fc41bc9ec3d1a02d6
Andrew McDowell <andrew.mcdowell@kablamo.com.au>
2021-03-18 16:41:31 +1100

    Adding glow line

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import * as React from 'react';
 import {useRef} from 'react';
-import {Entity, ModelGraphics, Viewer} from 'resium'
+import {Entity, ModelGraphics, Polyline, PolylineCollection, Viewer} from 'resium'
 import './App.css';
-import {Cartesian3} from 'cesium'
+import {Cartesian3, Color, Material} from 'cesium'
 import {useInterval} from "./useInterval";
 
+const GLOW_MATERIAL = Material.fromType(Material.PolylineGlowType, {
+    glowPower: 0.1,
+    color: new Color(1.0, 0.5, 0.0, 1.0)
+});
 
 function App() {
     const ref = useRef(null);
     const [position, setPosition] = React.useState(Cartesian3.fromDegrees(151.2093, -33.8688, 2000.0))
+    const [snailTrail, setSnailTrail] = React.useState([])
     useInterval(() => {
         const getISSPosition = async () => {
             const response = await fetch('https://api.wheretheiss.at/v1/satellites/25544')
             const json = await response.json()
             const {altitude, latitude, longitude} = json
+            const nextPosition = Cartesian3.fromDegrees(longitude, latitude, altitude * 1000)
             setPosition(Cartesian3.fromDegrees(longitude, latitude, altitude * 1000))
+            setSnailTrail([...snailTrail, nextPosition])
         }
         getISSPosition()
     }, 5000)
 
     return (
-        <Viewer ref={ref} style={{position:'absolute', top: 0, left: 0, right: 0, bottom: 0}}>
+        <Viewer ref={ref} style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0}}>
             <Entity position={position}>
                 <ModelGraphics uri={"/banana.gltf"} minimumPixelSize={200} maximumPixelSize={500}/>
             </Entity>
+            <PolylineCollection>
+                <Polyline positions={snailTrail} width={20} show={true} material={GLOW_MATERIAL}/>
+            </PolylineCollection>
         </Viewer>
     );
 }
